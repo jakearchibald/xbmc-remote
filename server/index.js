6fcffd09971cbc77fa05df24a88448eeee85e8a0
Jake Archibald <jaffathecake@gmail.com>
2014-03-05 10:42:21 +0000

    Avoiding template caching

--- server/index.js
+++ server/index.js
@@ -4,6 +4,9 @@ var swig = require('swig');
 var app = express();
 
 app.engine('html', swig.renderFile);
+app.set('view cache', false);
+swig.setDefaults({ cache: false });
+
 app.use('/static', express.static(__dirname + '/../www/static'));
 
 app.get('/', function(req, res) {
