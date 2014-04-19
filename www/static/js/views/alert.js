c8fb86b9d884139b38e80ed82292c2be829c7be5
Jake Archibald <jaffathecake@gmail.com>
2014-04-19 16:24:47 +0100

    Button styles in alerts

--- www/static/js/views/alert.js
+++ www/static/js/views/alert.js
@@ -1,4 +1,3 @@
-var EventEmitter = require('events').EventEmitter;
 var defaults = require('../utils').defaults;
 var strToEl = require('../utils').strToEl;
 var toArray = require('../utils').toArray;
@@ -8,20 +7,19 @@ var alertTemplate = require('./templates/alert.hbs');
 function Alert(title, text, buttons) {
   var thisAlert = this;
 
-  EventEmitter.call(this);
   this.el = strToEl(alertTemplate({
     title: title,
     text: text,
     buttons: buttons
   }));
 
-  toArray(this.el.querySelectorAll('.buttons button')).forEach(function(button, i) {
+  toArray(this.el.querySelectorAll('.button-row button')).forEach(function(button, i) {
     if (buttons[i].onclick) {
       button.addEventListener('click', buttons[i].onclick);
     }
   });
 }
 
-var AlertProto = Alert.prototype = Object.create(EventEmitter.prototype);
+var AlertProto = Alert.prototype;
 
 module.exports = Alert;
